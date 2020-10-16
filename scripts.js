0874df7818a1a7a8b63daa83eeda8a62ddb58fac
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-10-15 23:19:59 -0400

    First city in search loads, added background images, basic info present

--- scripts.js
+++ scripts.js
@@ -3,10 +3,20 @@
 let apiKey = "&units=metric&appid=73f750c25d847cd31222725efab61589";
 let apiCall = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-function search(){
+// Allow submit with "Enter key"
+let cityInput = document.getElementById("city-name");
+cityInput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("city-submit").click();
+    }
+});
+
+// use search input to call API
+function search() {
     city = document.getElementById("city-name").value;
     let searchURL = apiCall + city + apiKey;
-    
+
     console.log(city);
     console.log(searchURL);
 
@@ -15,15 +25,40 @@ function search(){
         .then((data) => {
             nameData = data.name;
             tempData = data.main.temp;
-            console.log("Name: ", nameData, "Temp: ", tempData);
+            typeData = data.weather[0].main;
+            tempHighData = data.main.temp_max;
+            tempLowData = data.main.temp_min;
             result();
         });
 };
 
-function result(){
+// use API retrieved of selected city to output results on page
+function result() {
+    document.getElementById("initial-container").style.display = "none";
+    document.getElementById("results").style.display = "content";
+
     let city = document.getElementById("city");
     city.textContent = nameData;
 
+    let type = document.getElementById("type");
+    type.textContent = "Current Weather: " + typeData;
+    image = "url('images/" + typeData + ".jpg')";
+    console.log(image);
+    document.body.style.backgroundImage = image;
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+
+    
     let temp = document.getElementById("temp");
-    temp.textContent = tempData;
+    temp.textContent = "Current Temperature: " + Math.round(tempData);    
+
+    let tempHigh = document.getElementById("high");
+    tempHigh.textContent = "High: " + Math.round(tempHighData);
+
+    let tempLow = document.getElementById("low");
+    tempLow.textContent = "Low: " + Math.round(tempLowData);
 };
+
+function reset() {
+    window.location = "index.html";
+}
